a8de6381929a780a0fa7824b28fc8e1113770aca
Lorenzo05-creator <lory4jz@gmail.com>
2026-03-18 11:20:13 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 
-Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
-
-Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
-Route::post('/register', [RegisteredUserController::class, 'store']);
 
 
 Route::middleware('auth')->group(function () {
